ac6d3a671a0c714c2bad1a6fad3d6b9fdab3ce4a
ceydacoskun <113719615+ceydacoskun@users.noreply.github.com>
2022-09-27 21:36:16 +0300

    2-week

---
+++ 2-Week homework/oop1/src/oop1/CorporateCustomer.java
@@ -0,0 +1,23 @@
+package oop1;
+
+public class CorporateCustomer extends customer {
+	private String companyName;
+	private String taxName;
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+
+	public String getTaxName() {
+		return taxName;
+	}
+
+	public void setTaxName(String taxName) {
+		this.taxName = taxName;
+	}
+
+}
